670e2d40a7343bb0194eaf295ed6263de85979d4
Daniel Firth <locallycompact@gmail.com>
2018-08-17 09:30:01 +0100

    Initial draft of using pandoc API instead of command line

--- Shakefile.hs
+++ Shakefile.hs
@@ -1,40 +1,81 @@
-import Control.Monad
+{-# LANGUAGE DeriveAnyClass      #-}
+{-# LANGUAGE DeriveGeneric       #-}
+{-# LANGUAGE FlexibleContexts    #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+import Control.Lens
+import Data.Default
+import Data.Yaml
 import Development.Shake
 import Development.Shake.Command
 import Development.Shake.FilePath
 import Development.Shake.Util
+import RIO hiding (view)
+import RIO.ByteString.Lazy as LBS
+import RIO.Text as Text
+import RIO.Set as Set
+import Slick
+import Text.Pandoc.Class
+import Text.Pandoc.PDF
+import Text.Pandoc.Options
+import Text.Pandoc.Templates
+import Text.Pandoc.Readers
+import Text.Pandoc.Writers
 
-site    = "public"
+site :: FilePath
+site = "public"
+
+browser :: FilePath
 browser = "chromium"
 
+meta :: [FilePath]
 meta  = ["meta.txt"]
 
-css   = ["css/*.css"]
+css :: [FilePath]
+css = ["css/*.css"]
+
+fonts :: [FilePath]
 fonts = ["fonts/*.ttf"]
+
+imgs :: [FilePath]
 imgs  = ["img/*.png"]
-js    = ["js/*.js"]
+
+js :: [FilePath]
+js  = ["js/*.js"]
+
+mdwn :: [FilePath]
 mdwn  = ["notes//*.md"]
 
+htemplate :: FilePath
 htemplate = "resources/page.tmpl"
 
+tocOpts :: [String]
 tocOpts = ["--toc", "--toc-depth=2"]
 
+margin :: String
 margin = "3cm"
 
 getCSSFiles      = getDirectoryFiles "" css
 getFonts         = getDirectoryFiles "" fonts
 getImages        = getDirectoryFiles "" imgs
 getJSFiles       = getDirectoryFiles "" js
-getMarkdownFiles = getDirectoryFiles "" mdwn
 
-supportingFile = flip copyFile' <*> dropDirectory1
+verbatim :: Iso' FilePath FilePath
+verbatim = iso (site </>) dropDirectory1
 
-carryToSite = map (site </>)
+copyVerbatim :: FilePath -> Action ()
+copyVerbatim = flip copyFile' <*> (view . from $ verbatim)
 
-pandoc out opts = do
-  mdwn <- getMarkdownFiles
-  need $ meta ++ mdwn
-  cmd "pandoc" (meta ++ mdwn) $ ["-o", out, "-s"] ++ opts
+pandocCmd :: [FilePath] -> [String] -> FilePath -> Action ()
+pandocCmd files opts out = command [] "pandoc" $ files <> ["-o", out, "-s"] <> opts
+
+compileMarkdown :: [FilePath] -> [String] -> FilePath -> Action ()
+compileMarkdown mdwn opts out = getDirectoryFiles "" mdwn >>= \x -> pandocCmd x opts out
+
+data Page = Page {
+  content :: Text
+} deriving (Eq, Show, Generic, FromJSON, ToJSON)
 
 main :: IO ()
 main = shakeArgs shakeOptions $ do
@@ -48,9 +89,10 @@ main = shakeArgs shakeOptions $ do
     putNormal $ "Cleaning files in " ++ site
     removeFilesAfter "." [site]
 
-  let supports = carryToSite $ join [css, fonts, imgs, js]
+  let supports  = join [css, fonts, imgs, js]
+  let supportsT = view verbatim <$> supports
 
-  forM supports $ flip (%>) supportingFile
+  forM_ supportsT $ flip (%>) copyVerbatim
 
   index %> \out -> do
     css   <- getCSSFiles
@@ -58,16 +100,24 @@ main = shakeArgs shakeOptions $ do
     imgs  <- getImages
     js    <- getJSFiles
     let cssOpts = css >>= (\x -> ["-c", x])
-    need $ carryToSite $ join [css, fonts, imgs, js]
+    need $ view verbatim <$> join [css, fonts, imgs, js]
     need [htemplate]
-    pandoc out $ ["--template", htemplate,
-                  "-t", "html", "-f", "markdown",
-                  "--highlight-style", "pygments",
-                  "--mathjax"] ++ cssOpts ++ tocOpts
+    let opts = ["--template", htemplate,
+                "-t", "html", "-f", "markdown",
+                "--highlight-style", "pygments",
+                "--mathjax"] ++ cssOpts ++ tocOpts
+    compileMarkdown mdwn opts out
 
-  pdf %> flip pandoc (tocOpts ++ ["--variable", "geometry:margin=" ++ margin])
+  pdf %> \out -> do
+    x <- getDirectoryFiles "" mdwn
+    y <- mapM readFile' $ meta <> x
+    f <- liftIO $ runIOorExplode $ do
+      p <- readMarkdown def {readerExtensions = pandocExtensions} (Text.pack $ join y)
+      d <- getDefaultTemplate "latex"
+      makePDF "pdflatex" [] writeLaTeX def {writerTemplate=Just d, writerTableOfContents = True} p
+    either (fail . show) return f >>=  LBS.writeFile out
 
-  beamer %> flip pandoc ["-t", "beamer"]
+  beamer %> compileMarkdown mdwn ["-t", "beamer"]
 
   phony "pdf"    $ need [pdf]
   phony "beamer" $ need [beamer]
