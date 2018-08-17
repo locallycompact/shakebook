651edae41642219f086be98ec6d1428006867512
Daniel Firth <locallycompact@gmail.com>
2018-08-17 17:25:37 +0100

    Simplify monad structure

--- Shakefile.hs
+++ Shakefile.hs
@@ -5,25 +5,25 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TupleSections #-}
 
-import Control.Lens
-import Data.Default
-import Data.Yaml
-import Development.Shake
-import Development.Shake.Command
-import Development.Shake.FilePath
-import Development.Shake.Util
-import RIO hiding (view)
-import qualified RIO.ByteString.Lazy as LBS
-import RIO.Text as Text
-import RIO.Set as Set
-import Slick
-import Text.Pandoc.Class
-import Text.Pandoc.Definition
-import Text.Pandoc.PDF
-import Text.Pandoc.Options
-import Text.Pandoc.Templates
-import Text.Pandoc.Readers
-import Text.Pandoc.Writers
+import           Control.Lens
+import           Data.Default
+import           Data.Yaml
+import           Development.Shake
+import           Development.Shake.Command
+import           Development.Shake.FilePath
+import           Development.Shake.Util
+import           RIO                     hiding ( view )
+import qualified RIO.ByteString.Lazy           as LBS
+import           RIO.Text                      as Text
+import           RIO.Set                       as Set
+import           Slick
+import           Text.Pandoc.Class
+import           Text.Pandoc.Definition
+import           Text.Pandoc.PDF
+import           Text.Pandoc.Options
+import           Text.Pandoc.Templates
+import           Text.Pandoc.Readers
+import           Text.Pandoc.Writers
 
 --- Global Configuration -----------------------------------------------------------
 
@@ -34,7 +34,7 @@ browser :: FilePath
 browser = "chromium"
 
 meta :: [FilePattern]
-meta  = ["meta.txt"]
+meta = ["meta.txt"]
 
 css :: [FilePattern]
 css = ["css/*.css"]
@@ -43,13 +43,13 @@ fonts :: [FilePattern]
 fonts = ["fonts/*.ttf"]
 
 imgs :: [FilePattern]
-imgs  = ["img/*.png"]
+imgs = ["img/*.png"]
 
 js :: [FilePattern]
-js  = ["js/*.js"]
+js = ["js/*.js"]
 
 mdwn :: [FilePattern]
-mdwn  = ["notes//*.md"]
+mdwn = ["notes//*.md"]
 
 htemplate :: FilePath
 htemplate = "resources/page.tmpl"
@@ -65,15 +65,15 @@ verbatim = iso (site </>) dropDirectory1
 copyVerbatim :: FilePath -> Action ()
 copyVerbatim = flip copyFile' <*> (view . from $ verbatim)
 
-getCSSFiles      = getDirectoryFiles "" css
-getFonts         = getDirectoryFiles "" fonts
-getImages        = getDirectoryFiles "" imgs
-getJSFiles       = getDirectoryFiles "" js
+getCSSFiles = getDirectoryFiles "" css
+getFonts = getDirectoryFiles "" fonts
+getImages = getDirectoryFiles "" imgs
+getJSFiles = getDirectoryFiles "" js
 
 --- Pandoc Options -------------------------------------------------------------
 
 markdownReaderOptions :: ReaderOptions
-markdownReaderOptions = def {readerExtensions = pandocExtensions }
+markdownReaderOptions = def { readerExtensions = pandocExtensions }
 
 latexWriterOptions :: WriterOptions
 latexWriterOptions = def { writerTableOfContents = True }
@@ -86,31 +86,23 @@ html5WriterOptions = def { writerTableOfContents = True }
 
 --- PDF Compilation ------------------------------------------------------------
 
-compileMarkdown mdwn act out = do
-  x <- getDirectoryFiles "" mdwn
-  y <- mapM readFile' x
-  f <- act . Text.pack . join $ y
-  LBS.writeFile out f
+makePDFThrow :: String -> [String] -> (WriterOptions -> Pandoc -> PandocIO Text) -> WriterOptions -> Pandoc -> PandocIO LBS.ByteString
+makePDFThrow a b c d e = makePDF a b c d e >>= either (fail . show) return
 
-makePDFWith mdwn writer writerOptions template = compileMarkdown mdwn $ \x -> do
-  f <- liftIO . runIOorExplode $ do
-    p <- readMarkdown markdownReaderOptions x
-    k <- template
-    makePDF "pdflatex" [] writer writerOptions { writerTemplate = Just k } p
-  either (fail . show) return f
+makePDFBeamer :: Pandoc -> PandocIO LBS.ByteString
+makePDFBeamer p = do
+  t <- getDefaultTemplate "beamer"
+  makePDFThrow "pdflatex" [] writeBeamer beamerWriterOptions { writerTemplate = Just t } p
 
-makePDFBeamer :: [FilePattern] -> FilePath -> Action ()
-makePDFBeamer mdwn = makePDFWith mdwn writeBeamer beamerWriterOptions (getDefaultTemplate "beamer")
-
-makePDFLaTeX :: [FilePattern] -> FilePath -> Action ()
-makePDFLaTeX mdwn = makePDFWith mdwn writeLaTeX latexWriterOptions (getDefaultTemplate "latex")
+makePDFLaTeX :: Pandoc -> PandocIO LBS.ByteString
+makePDFLaTeX p = do
+  t <- getDefaultTemplate "latex"
+  makePDFThrow "pdflatex" [] writeLaTeX latexWriterOptions { writerTemplate = Just t } p
 
 --- HTML Compilation --------------------------------------------------------
 
-makeHTML5With mdwn writer writerOptions template = compileMarkdown mdwn $ \x -> do
-  k <- template
-  (v :: Page) <- loadUsing' (readMarkdown markdownReaderOptions)
-                             (writer writerOptions { writerTemplate = Just k }) x
+makeHTML5Page mdwn writerOptions = do
+  (v :: Page) <- loadUsing' (readMarkdown markdownReaderOptions) (writeHtml5String writerOptions) mdwn
   return $ LBS.fromStrict $ encodeUtf8 (content v)
 
 data Page = Page {
@@ -142,13 +134,29 @@ main = shakeArgs shakeOptions $ do
     imgs  <- getImages
     js    <- getJSFiles
     need $ view verbatim <$> join [css, fonts, imgs, js]
-    need [htemplate]
-    makeHTML5With (mdwn <> meta) writeHtml5String html5WriterOptions { writerVariables = ("css",) <$> css } (readFile' htemplate) out
+    x <- getDirectoryFiles "" $ mdwn <> meta
+    y <- mapM readFile' x
+    t <- readFile' htemplate
+    f <- makeHTML5Page (Text.pack . join $ y)
+                       html5WriterOptions { writerVariables = ("css", ) <$> css , writerTemplate = Just t }
+    LBS.writeFile out f
 
-  pdf %> makePDFLaTeX (mdwn <> meta)
+  pdf %> \out -> do
+    x <- getDirectoryFiles "" $ mdwn <> meta
+    y <- mapM readFile' x
+    f <- liftIO . runIOorExplode $ do
+      p <- readMarkdown markdownReaderOptions $ Text.pack . join $ y
+      makePDFLaTeX p
+    LBS.writeFile out f
 
-  beamer %> makePDFBeamer (mdwn <> meta)
+  beamer %> \out -> do
+    x <- getDirectoryFiles "" $ mdwn <> meta
+    y <- mapM readFile' x 
+    f <- liftIO . runIOorExplode $ do
+      p <- readMarkdown markdownReaderOptions $ Text.pack . join $ y
+      makePDFBeamer p
+    LBS.writeFile out f
 
-  phony "pdf"    $ need [pdf]
+  phony "pdf" $ need [pdf]
   phony "beamer" $ need [beamer]
-  phony "test"   $ need [index] >> cmd browser index
+  phony "test" $ need [index] >> cmd browser index
