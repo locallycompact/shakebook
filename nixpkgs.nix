2b16f6d195ba2230949a6565ec97ca17654c4b59
Daniel Firth <locallycompact@gmail.com>
2019-01-02 11:01:49 +0000

    Bump nixpkgs

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,5 +1,5 @@
 import (builtins.fetchTarball {
-  name = "nixos-unstable-2018-12-12";
-  url = "https://github.com/nixos/nixpkgs/archive/47f008676fe3b77b9c8edda54db621a0dc16dd8e.tar.gz";
-  sha256 = "1mrpavv63nc5kfg5cya9z6008yqgni8i8s4kv6facww1mmn8vvsr";
+  name = "nixos-unstable-2018-12-25";
+  url = "https://github.com/nixos/nixpkgs/archive/aac952c48a2a827240f19ed190f668b2e407de55.tar.gz";
+  sha256 = "0v0d66xh1ag0f056sk99v021fc2qia9mnn67ym88yw9w0jlnw1n0";
 })
